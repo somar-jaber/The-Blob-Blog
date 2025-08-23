ee6d264b33db4e6cbe2207dff1aa65b89199c3a9
somar-jaber <brh99425@gmail.com>
2025-08-23 14:30:34 +0300

    ..

---
+++ .eleventy.js
@@ -0,0 +1,46 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+
+module.exports = function(eleventyConfig) {
+    // 11ty takes .html , .md and other files automatically when run but not the css and the media files, so we need to tell it to take them.
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    // I have subfolders in pages, 11th wasn't including them , so i added this to fix the problem
+    eleventyConfig.addPassthroughCopy("./src/pages/");
+
+    // To fix how 11ty displays time
+    eleventyConfig.addFilter("postDate" , (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    // Markdown filter to render markdown inside html
+    const md = new markdownIt({
+        html: true
+    });
+    eleventyConfig.addPairedShortcode("markdown", (content) => {
+        return md.render(content);
+    });
+
+    // applying highlighting 
+    eleventyConfig.addPlugin(syntaxHighlight);  
+
+    // For fixing the absolute url problem in github pages, this is a filter. names "url"
+    pathPrefix: process.env.ELEVENTY_ENV === "production" ? "/The-Blob-Blog/" : ""
+
+    // Setting up what is the input and output directories 
+    return {
+        // configure 11ty to use Nunjucks as the template enging insted of the default one : Liquid 
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+
+        dir: {
+            input: "src",
+            output: "_site"
+        },
+    };
+}
+
+
---
+++ src/index.md
@@ -0,0 +1,65 @@
+---
+layout : "base-layout.njk"
+---
+
+
+# The Blob Blog
+*we love* <span class="hashtag">#ComputerScience</span> <span class="hashtag">#Offline</span> <span class="hashtag">#Digital Content</span> <span class="hashtag">#Tech</span>
+
+<br>
+<br>
+<hr>
+<br>
+
+## **Welcome to** The Blob Blog
+<p>
+    &nbsp; &nbsp; Where we provide a technical help , advices , and suggestions
+    in the computer's and digital's world.
+</p>
+
+<br>
+<br>
+<br>
+
+## The Index
+- [What does Blob means ?](/pages/blob)
+- [Highlight code in 11ty](/pages/11ty/highlight-code)
+- [Deep to the Context menu of directories](/pages/deep-to-the-context-menu)
+- [CS Goes Crazy](/pages/cs-goes-crazy)
+- [Wallpapers Websites for PC](/pages/wallpapers-websites-for-pc)
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+**Made with &#128420;, by Somar** <br />
+**Thanks for [11ty](https://jamstack.org/generators/eleventy/) project to make this blog easy and possible.**
+22/08/2025
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
